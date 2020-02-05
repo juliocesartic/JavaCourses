0f233da9a37a67dd35bbc0ec0a3a7c192394972d
Julio Jaramillo <juliodev@mx1.ibm.com>
2020-02-04 21:47:03 -0600

    Step 6: @Primary

--- demo/src/main/java/com/example/demo/BubbleSortAlgorithm.java
+++ demo/src/main/java/com/example/demo/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	public int [] sort(int [] numbers) {
 		//Write the logic of BubbleSort
--- demo/src/main/java/com/example/demo/QuickSortAlgorithm.java
+++ demo/src/main/java/com/example/demo/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm {
 	public int [] sort(int [] numbers) {
 		//Write the logic of Quick Sort
