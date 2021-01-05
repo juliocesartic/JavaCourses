1b7ff58d64c850d446c9f7aa792e45c0ca02b496
Julio Jaramillo <jcjaramillo@hotmail.com>
2021-01-04 21:25:43 -0600

    Mejorando la API: Controllers and Documentation

---
+++ platzi-market/src/main/java/com/platzi/market/domain/service/PurchaseService.java
@@ -0,0 +1,31 @@
+package com.platzi.market.domain.service;
+
+
+import com.platzi.market.domain.Purchase;
+import com.platzi.market.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+
+    @Autowired
+    private PurchaseRepository repository;
+
+    public List<Purchase> getAll(){
+        return repository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clientId){
+        return repository.getByClient(clientId);
+    }
+
+    public Purchase save(Purchase purchase){
+        return repository.save(purchase);
+    }
+
+
+}
--- platzi-market/src/main/java/com/platzi/market/web/controller/ProductController.java
+++ platzi-market/src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -2,7 +2,13 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +22,44 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    public List<Product> getAll(){
-        return productService.getAll();
+    @ApiOperation("Get All supermarket products")
+    @ApiResponse(code = 200, message = "OK")
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") int productId){
-        return productService.getProduct(productId);
+    @ApiOperation("Search a product by ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(@ApiParam(value="Product ID", required=true, example = "7")
+                                                  @PathVariable("id") int productId){
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
-        return productService.getByCategory(categoryId);
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+        return productService.getByCategory(categoryId)
+                .map(productsByCategory -> new ResponseEntity<>(productsByCategory, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    public Product save(@RequestBody Product product){
-        return productService.save(product);
+    public ResponseEntity<Product> save(@RequestBody Product product){
+        return new ResponseEntity<>(productService.save(product), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public boolean delete(@PathVariable("id") int productId){
-        return productService.delete(productId);
+    public ResponseEntity delete(@PathVariable("id") int productId){
+        if(productService.delete(productId)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
